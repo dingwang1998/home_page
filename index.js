0d2478645ccb0c380a19a72c335fdce9ed0741af
一路有你 <1208686840@qq.com>
2020-04-05 15:31:27 +0800

    完成啦

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const headerEl = document.querySelector("header")
+const scrollTop = document.querySelector(".scrollToTop")
+
+window.addEventListener("scroll",()=>{
+    let height = headerEl.getBoundingClientRect().height
+
+    if(window.pageYOffset - height >0){
+        if(!headerEl.classList.contains("sticky")){
+            headerEl.classList.add("sticky")
+        }
+    }else {
+        headerEl.classList.remove("sticky")
+    }
+
+    if(window.pageYOffset > 2000){
+        scrollTop.style.display = "block"
+    }else{
+        scrollTop.style.display = "none"
+    }
+})
+
+
 const glide = new Glide(".glide");
 const captionsEl = document.querySelectorAll(".slide-caption")
 
@@ -39,4 +61,58 @@ filterBtns.addEventListener("click",e => {
         isotope.arrange({filter:filterOption});    
     }
 })
-    
+ 
+const staggeringOption = {
+    delay:300,
+    distance:"50px",
+    duration:500,
+    easing:"ease-in-out",
+    origin:"bottom"
+};
+
+ScrollReveal().reveal(".feature",{...staggeringOption, interval: 350});
+
+ScrollReveal().reveal(".service-item",{...staggeringOption, interval: 350});
+
+ScrollReveal().reveal(".data-section",{
+    beforeReveal:() =>{
+        anime({
+            targets:".data-price .num",
+            innerHTML: el=>{
+                return [0 , el.innerHTML]
+            },
+            duration:2000,
+            round:1,
+            easing:"easeInExpo"
+        })
+    }
+})
+
+// 滚动特效
+const scroll = new SmoothScroll('nav a[href*= "#"], .scrollToTop a[href*="#"]',{
+    header:"header",
+    offset:80
+})
+
+
+// 滚动关闭open
+document.addEventListener("scrollStart",()=>{
+    if(headerEl.classList.contains("open")){
+        headerEl.classList.remove("open")
+    }
+})
+
+const exploreBtnEls = document.querySelectorAll(".explore-btn");
+
+exploreBtnEls.forEach(exploreBtnEl =>{
+    exploreBtnEl.addEventListener("click",()=>{
+        scroll.animateScroll(document.querySelector("#about-us"));
+    });
+});
+
+// 折叠按钮
+const burgerEl = document.querySelector(".burger");
+
+burgerEl.addEventListener("click",()=>{
+    headerEl.classList.toggle("open")
+})
